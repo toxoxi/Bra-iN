a8abddf9427c2292e7a957c031f0d4af7740a472
Yorori <yorori.too@gmail.com>
2018-08-02 22:43:01 +0900

    add: make font-awesome available

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+import "@fortawesome/fontawesome-free-webfonts/css/fontawesome.css";
+import "@fortawesome/fontawesome-free-webfonts/css/fa-brands.css";
+import "@fortawesome/fontawesome-free-webfonts/css/fa-regular.css";
+import "@fortawesome/fontawesome-free-webfonts/css/fa-solid.css";
+
 Vue.config.productionTip = false;
 
 new Vue({
