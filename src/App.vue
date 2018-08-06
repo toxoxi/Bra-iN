99fc990320bf755b5498005ea558de3e6482d3da
Yorori <yorori.too@gmail.com>
2018-08-06 21:57:07 +0900

    update: apply buefy instead of bulma

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,6 @@
 </template>
 
 <style lang="scss">
-@import "../node_modules/bulma/bulma.sass";
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import Buefy from "buefy";
+import "buefy/lib/buefy.css";
 
 import "@fortawesome/fontawesome-free-webfonts/css/fontawesome.css";
 import "@fortawesome/fontawesome-free-webfonts/css/fa-brands.css";
@@ -8,6 +10,7 @@ import "@fortawesome/fontawesome-free-webfonts/css/fa-regular.css";
 import "@fortawesome/fontawesome-free-webfonts/css/fa-solid.css";
 
 Vue.config.productionTip = false;
+Vue.use(Buefy);
 
 new Vue({
   router,
