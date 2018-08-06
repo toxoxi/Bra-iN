36c9425375f9296e4a77ce4f390bbe5a52287626
Yorori <yorori.too@gmail.com>
2018-08-06 20:22:19 +0900

    add: make mavonEditor available

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Editor></Editor>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import Editor from "@/components/Editor.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    HelloWorld,
+    Editor
   }
 };
 </script>
