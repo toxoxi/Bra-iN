3638859a395ed3b768ef8d42dcaa299de2cb380e
Yorori <yorori.too@gmail.com>
2018-08-26 22:21:40 +0900

    fix: Fix problem can not deal with multiple entry points

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,16 @@ module.exports = {
     signin: {
       entry: 'src/entry-point/signin/main.js',
       template: 'public/signin.html',
-      filename: 'signin.html'
+      filename: 'signin.html',
+      title: 'Signin page'
+    }
+  },
+  devServer: {
+    historyApiFallback: {
+      rewrites: [
+        { from: /\/index/, to: '/index.html' },
+        { from: /\/signin/, to: '/signin.html' }
+      ]
     }
   }
 }
