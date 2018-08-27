4c17896dbfb2b9ec791b0e86f8b4927d10b18a13
Yorori <yorori.too@gmail.com>
2018-08-27 22:58:28 +0900

    add: Add router.beforeEach for check authentication when access to each page

--- src/routing/router.js
+++ src/routing/router.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import * as firebase from 'firebase/app'
+import 'firebase/auth'
 import IndexView from '@/views/IndexView'
 import ListView from '@/views/ListView'
 import NoteView from '@/views/NoteView'
@@ -7,7 +9,7 @@ import NotFoundView from '@/views/NotFoundView'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
@@ -19,12 +21,18 @@ export default new Router({
     {
       path: '/list',
       name: 'ListView',
-      component: ListView
+      component: ListView,
+      meta: {
+        requireAuth: true
+      }
     },
     {
       path: '/note/:id',
       name: 'NoteView',
-      component: NoteView
+      component: NoteView,
+      meta: {
+        requireAuth: true
+      }
     },
     {
       path: '*',
@@ -36,3 +44,27 @@ export default new Router({
     return savedPosition ? savedPosition : { x: 0, y: 0 }
   }
 })
+
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some(record => record.meta.requireAuth)
+  if (requiresAuth) {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        next()
+      } else {
+        // fullPathから 最初の '/' を取り除く
+        const fromPath = from.fullPath.split('/')
+        fromPath.shift()
+        // index('/')から飛ぶ場合は明示的に'index'を代入
+        const fromView = fromPath[0] === '' ? 'index' : fromPath[0]
+
+        next(false) // next(false) でルーターのフックを解決する
+        location.replace(`/signin?p=${fromView}`)
+      }
+    })
+  } else {
+    next()
+  }
+})
+
+export default router
